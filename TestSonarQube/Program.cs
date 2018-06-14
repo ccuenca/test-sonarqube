0fc5150ef1bb42bb089e67cbcd64cde269e0c371
Cristhian <cristhian@MacBook-Pro-de-Cristhian.local>
2018-06-14 00:01:55 -0500

    changes

--- TestSonarQube/Program.cs
+++ TestSonarQube/Program.cs
@@ -4,14 +4,16 @@ namespace TestSonarQube
 {
     class Program
     {
+        protected Program(){
+            
+        }
+        
         static void Main(string[] args)
         {
             //esta es una prueba
 
             Console.WriteLine("Hello World!");
-
-            //Console.WriteLine("Hello World!");
-            //Console.WriteLine("Hello World!");
+              
         }
     }
 }
