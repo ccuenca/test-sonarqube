197e10359c6935f5c81cf02c8ceeeb5306dd62d5
Cristhian Cuenca <ccuenca24@gmail.com>
2018-06-14 09:53:08 -0500

    changes

--- TestSonarQube/Program.cs
+++ TestSonarQube/Program.cs
@@ -9,13 +9,8 @@ namespace TestSonarQube
         }
         
         static void Main(string[] args)
-        {
-            //esta es una prueba
-
+        {            
             Console.WriteLine("Hello World!");
-			
-			//Console.WriteLine("Hello World!");
-              
         }
     }
 }
